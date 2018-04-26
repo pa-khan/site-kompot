d044a4c9552d660ac92b12b096869c354143d3a8
e5ash <io@e5ash.com>
2018-04-27 00:14:07 +0300

    fix

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -1,5 +1,23 @@
 $(document).ready(function($) {
 
-	$('.text_phone').mask('+7 (999) 999-99-99');
+	$('.input_tel input').mask('+7 (999) 999-99-99');
 	
+	$('.reviews__list').slick({
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		arrows: false,
+		dots: true,
+		responsive: [{
+      breakpoint: 787,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+	})
+	 $(".panel__nav a[href^='#']").click(function(){
+      var _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top - 50 +"px"}, 1500);
+      return false;
+	  });
 });
